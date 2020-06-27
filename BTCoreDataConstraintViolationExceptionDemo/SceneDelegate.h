abde8947befb557dee9764d99adfa8e8f4ae070d
Daria Kopaliani <daria.kopaliani@gmail.com>
2020-06-27 18:03:19 +0300

    Add `BTCoreDataConstraintViolationExceptionDemo`

---
+++ BTCoreDataConstraintViolationExceptionDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  BTCoreDataConstraintViolationExceptionDemo
+//
+//  Created by Daria on 27.06.2020.
+//  Copyright © 2020 Beat Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ BTCoreDataConstraintViolationExceptionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BTCoreDataConstraintViolationExceptionDemo
+//
+//  Created by Daria on 27.06.2020.
+//  Copyright © 2020 Beat Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
